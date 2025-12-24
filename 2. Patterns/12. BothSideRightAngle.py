40c94f07dcd80f675c9e673bd5e2058db83baeb4
anupamyadavanurag <anuragyadavanupam@gmail.com>
2025-12-25 00:36:19 +0530

    done

--- 2. Patterns/12. BothSideRightAngle.py
+++ 2. Patterns/12. BothSideRightAngle.py
@@ -1,11 +1,10 @@
-n = 4
-for i in range(1, n + 1):
+for i in range(1, 4 + 1):
     # Left numbers
     for j in range(1, i + 1):
         print(j, end=" ")
     
     # Middle spaces
-    print("  " * (2 * (n - i)), end="")
+    print("  " * (2 * (4 - i)), end="")
     
     # Right numbers
     for j in range(1, i + 1):
