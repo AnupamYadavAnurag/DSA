ab6ac83caa34a6270eb2d38db9e25a5400635cfd
anupamyadavanurag <anuragyadavanupam@gmail.com>
2026-01-05 23:08:43 +0530

    done

--- 4. Maths/5. ArmstrongNumber.py
+++ 4. Maths/5. ArmstrongNumber.py
@@ -1,1 +1,14 @@
-num = input("Enter the no.")
+num = int(input("Enter a number: "))
+temp = num
+n = len(str(num))
+sum = 0
+
+while temp > 0:
+    digit = temp % 10
+    sum += digit ** n
+    temp //= 10
+
+if sum == num:
+    print("Armstrong Number")
+else:
+    print("Not an Armstrong Number")
