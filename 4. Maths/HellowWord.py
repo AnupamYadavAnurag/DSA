89465e9cc9da09392fa0dbd6472937ecdf71ec9d
anupamyadavanurag <anuragyadavanupam@gmail.com>
2026-01-14 11:52:14 +0530

    done

---
+++ 4. Maths/HellowWord.py
@@ -0,0 +1,3 @@
+for i in [72, 101, 108, 108, 111]:
+    print(chr(i), end='')
+print()
---
+++ 4. Maths/Rough Practice/PositiveORNegitiveUsingTernary.py
@@ -0,0 +1,4 @@
+num = int(input("Enter a number: "))
+
+result = "Positive" if num > 0 else "Negative"
+print(result)
