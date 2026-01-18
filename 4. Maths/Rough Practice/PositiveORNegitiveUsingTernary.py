2b6748d8c23d856a74a9f8e6bc0007fe80a13f9e
anupamyadavanurag <anuragyadavanupam@gmail.com>
2026-01-18 23:57:38 +0530

    done

--- 4. Maths/Rough Practice/PositiveORNegitiveUsingTernary.py
+++ 4. Maths/Rough Practice/PositiveORNegitiveUsingTernary.py
@@ -2,3 +2,7 @@ num = int(input("Enter a number: "))
 
 result = "Positive" if num > 0 else "Negative"
 print(result)
+num = int(input("Enter a number: "))
+
+result = "Positive" if num > 0 else "Negative"
+print(result)
