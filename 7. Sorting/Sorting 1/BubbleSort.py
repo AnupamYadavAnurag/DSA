87a737c60fa17359f1f7abf3b58d16feda97bc01
anupamyadavanurag <anuragyadavanupam@gmail.com>
2026-02-02 23:48:45 +0530

    done

--- 7. Sorting/Sorting 1/BubbleSort.py
+++ 7. Sorting/Sorting 1/BubbleSort.py
@@ -5,5 +5,4 @@ for i in range(n):
     for j in range(0, n - i - 1):
         if arr[j] > arr[j + 1]:
             arr[j], arr[j + 1] = arr[j + 1], arr[j]
-
 print("Sorted array:", arr)
--- 7. Sorting/Sorting 1/InsertionSort.py
+++ 7. Sorting/Sorting 1/InsertionSort.py
@@ -8,7 +8,6 @@ for i in range(1, n):
     while j >= 0 and arr[j] > key:
         arr[j + 1] = arr[j]
         j -= 1
-
-    arr[j + 1] = key
+        arr[j + 1] = key
 
 print("Sorted array:", arr)
